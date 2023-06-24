a7c028991fb84ed7abc548d99d87a1bb81d3e746
Shourov Foisal <shourovfoisal@gmail.com>
2023-06-24 14:40:37 +0600

    Tried fetch api

--- src/components/FetchApi.tsx
+++ src/components/FetchApi.tsx
@@ -1,6 +1,22 @@
+import { useEffect, useState } from "react"
+import { Quote } from "../types/Quote";
 
 export const FetchApi = () => {
+
+  const [quote, setQuote] = useState<Quote>();
+
+  useEffect(() => {
+    const fetchQuote = async () => {
+      const response: Response = await fetch("https://api.quotable.io/random");
+      const data: Quote = await response.json();
+      setQuote(data);
+    }
+    fetchQuote();
+  }, []);
+
   return (
-    <div>FetchApi</div>
+    <div>
+      {quote?.content}
+    </div>
   )
 }
