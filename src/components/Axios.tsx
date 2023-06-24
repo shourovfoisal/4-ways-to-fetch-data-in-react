5b6bccee2cbf91fb7162a51de2db5971ecb7e212
Shourov Foisal <shourovfoisal@gmail.com>
2023-06-24 14:51:34 +0600

    Tried axios

--- src/components/Axios.tsx
+++ src/components/Axios.tsx
@@ -1,6 +1,20 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Quote } from "../types/Quote";
 
 export const Axios = () => {
+
+  const [quote, setQuote] = useState<Quote>();
+
+  useEffect(() => {
+    const fetchQuote = async () => {
+      const response = await axios.get("https://api.quotable.io/random");
+      setQuote(response.data);
+    }
+    fetchQuote();
+  }, []);
+
   return (
-    <div>Axios</div>
+    <div>{quote?.content}</div>
   )
 }
